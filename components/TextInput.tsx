8cf3f368b0773bd9da0301b910caf8aa49ee5fda
sakuru9 <miokillian@gmail.com>
2021-03-12 23:46:31 +0200

    fix: remove broken font

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -9,7 +9,6 @@ const StyledTextInput = styled.TextInput`
   font-size: 20px;
   padding-left: 10px;
   background-color: rgb(36, 37, 47);
-  font-family: arial;
   color: rgb(255, 64, 102);
   border-bottom-color: rgb(238, 111, 135);
   border-width: 3px;
