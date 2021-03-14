6f415330e20ccb13e779f6f568aca9f0f9d879a6
sakuru9 <miokillian@gmail.com>
2021-03-14 03:33:45 +0200

    feat: some changes

--- components/StatusText.tsx
+++ components/StatusText.tsx
@@ -1,27 +1,25 @@
 import React from "react";
 import styled from "styled-components/native";
+import { Text } from "react-native";
 
-type NormalStatus = "NORMAL";
-type ErrorStatus = "ERROR";
-type SuccessStatus = "SUCCESS";
 type StatusMessageType = "NORMAL" | "ERROR" | "SUCCESS";
 
 interface StatusTextProps {
-  msg: string;
+  msg?: string;
   type: StatusMessageType;
 }
 
 const StyledStatusText = styled.Text<StatusTextProps>`
   color: ${(props) =>
-    props.type === "NORMAL"
-      ? props.theme.textColorRed1
+    props.type === "SUCCESS"
+      ? props.theme.successColor
       : props.type === "ERROR"
       ? props.theme.errorColor
       : props.theme.textColorRed1};
 `;
 
 const ErrorText = (props: StatusTextProps) => {
-  return <StyledStatusText>{props.msg}</StyledStatusText>;
+  return <StyledStatusText type={props.type}>{props.msg}</StyledStatusText>;
 };
 
 export default ErrorText;
