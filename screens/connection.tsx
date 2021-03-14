a4bad554cbc4261333bd8629a24a31f42fd6ea02
sakuru9 <miokillian@gmail.com>
2021-03-14 03:28:59 +0200

    feat: test ip address with connection check and inform status to user

--- screens/connection.tsx
+++ screens/connection.tsx
@@ -1,40 +1,67 @@
 import React, { useState } from "react";
 import { View, Button, Text } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { getScenes } from "../control/index";
+import { testBridgeConnection } from "../control/index";
 import { HueScene } from "../control/types/scenes";
-import { styles } from "../App";
+import { styles } from "../styles";
 import TextField from "../components/TextInput";
 import { withTheme } from "styled-components";
 import { validateIpAddress } from "../utils";
 import { pipe } from "fp-ts/lib/function";
 import * as E from "fp-ts/lib/Either";
-import ErrorText from "../components/ErrorText";
+import StatusText from "../components/StatusText";
 
 type IpValidateResult = false | string;
 
 const ConnectionScreen = () => {
   const [connStatus, setConnStatus] = useState(false);
-  const [errorMsg, setErrorMsg] = useState("");
+  const [statusMsg, setStatusMsg] = useState("");
   const [ip, setIp] = useState("");
+  const [connected, setConnected] = useState(false);
 
-  const checkConnection = (ipaddr: string) => {
-    const validate = pipe(
-      ipaddr,
+  const validateIpString = (ip: string) => {
+    return pipe(
+      ip,
       validateIpAddress,
+      E.map((ip) => ip),
       E.fold(
-        (l) => setErrorMsg("Invalid Ip address"),
+        (e) => e,
         (r) => r as IpValidateResult
       )
     );
+  };
 
-    // TODO
-    // connect to HUE API
+  const checkConnection = async (ipaddr: string) => {
+    setStatusMsg("");
+    setConnected(false);
+
+    const ipCheck = validateIpString(ipaddr);
+    if (ipCheck) {
+      const connCheck = pipe(
+        await testBridgeConnection(ipCheck),
+        E.map((res) => res as IpValidateResult),
+        E.fold(
+          (e) => {
+            setStatusMsg("Connection failed");
+            return false;
+          },
+          (r) => {
+            setStatusMsg("Connected successfully!");
+            setConnected(true);
+            return true;
+          }
+        )
+      );
+    } else {
+      setStatusMsg("Invalid IP address");
+    }
   };
 
   return (
     <View style={styles.container}>
-      {errorMsg.length > 0 && <ErrorText errorMsg={errorMsg} />}
+      {statusMsg.length > 0 && (
+        <StatusText type={connected ? "SUCCESS" : "ERROR"} msg={statusMsg} />
+      )}
       <TextField onChange={(str) => setIp(str)} />
       <Button title="Check connection" onPress={() => checkConnection(ip)} />
       <StatusBar style="dark" />
--- theme.ts
+++ theme.ts
@@ -2,4 +2,5 @@ export const Theme = {
   backgroundColor: "#24252f",
   textColorRed1: "#ee6f86",
   errorColor: "#fc033d",
+  successColor: "#32a852",
 };
